00aeba2d9da0fc3a8199f8e8e13e89459320bf3f
MerkiZ <merkizd@yandex.ru>
2023-06-28 20:49:24 +0300

    bot_changed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import requests
-import json
-
+import requests, json
 from tokens import user_token
+from tqdm import tqdm
+
 
 class VK_Parse:
     def __init__(self, access_token, age_from, age_to, sex):
@@ -20,14 +20,14 @@ class VK_Parse:
         else:
             res = result['response']['items']
             json_list = []
-            for item in res:
+            for item in tqdm(res, desc='Идет поиск...'):
                 profile_url = f"https://vk.com/id{item['id']}"
                 json_list.append({'id': item['id'], 'first_name': item['first_name'], 'last_name': item['last_name'], 'profile_url': profile_url})
 
                 photos = self.get_photos(item['id'])
                 json_list[-1]['photos'] = photos
 
-            with open('pair_data.json', 'w', encoding='UTF-8') as jsonfile:
+            with open('sql/json_data/pair_data.json', 'w', encoding='UTF-8') as jsonfile:
                 json.dump(json_list, jsonfile, ensure_ascii=False, indent=2)
 
     def get_photos(self, user_id):
@@ -44,5 +44,5 @@ class VK_Parse:
             print(f"Ошибка при получении фотографий: {e}")
             return []
 
-vk_parse = VK_Parse(user_token, 18, 30, 1)
-vk_parse.parse()
+# vk_parse = VK_Parse(user_token, 18, 30, 1)
+# vk_parse.parse()
